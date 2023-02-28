ac12a01345ee41201ce6b67f0cff26288a70d3a7
Hamid Mosalla <hamid.mosalla@lexisnexis.fr>
2023-02-28 17:23:42 +0100

    add benchmark

--- DotnetCoreBenchmark/Program.cs
+++ DotnetCoreBenchmark/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BenchmarkDotNet.Running;
+using DotnetCoreBenchmark.Benchmarks;
+// dotnet run -c Release // run in the command line
+var summary = BenchmarkRunner.Run<Md5VsSha256>();
